732bc044ff816a5b394d14e280f0b1b9b702df6b
yeakhil1 <yeakhil1@in.ibm.com>
2019-02-27 20:41:19 +0530

    commited

---
+++ src/app/quizify/tsclasses/register.ts
@@ -0,0 +1,14 @@
+import { Genre } from './Genre';
+import { Topic } from './topic';
+
+export class Register {
+
+    userName: string;
+    password: string;
+    confirmPassword: string;
+    emailId: string;
+    topic: Topic;
+    genres: Genre[];
+    gender: string;
+
+}
